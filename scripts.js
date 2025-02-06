e5934c2eec6f502612a19a13b7898addc8f80fb4
agawebnem <132296976+agawebnem@users.noreply.github.com>
2025-02-06 15:52:47 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Suits Token website loaded!");
+    // Any additional interactivity can be added here, like smooth scroll or animations.
+});
